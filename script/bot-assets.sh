60b88fc646c2e854ab3c91bfcf7a18438facbcec
myeong-han <ames11118@gmail.com>
2024-09-07 17:22:04 +0900

    update asset bot
    Signed-off-by: myeong-han <ames11118@gmail.com>

--- script/bot-assets.sh
+++ script/bot-assets.sh
@@ -16,18 +16,38 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $SCRIPT_DIR/../assets
 
-pages=($(find . -type d -maxdepth 1 | tail -n$(expr $(find . -type d -maxdepth 1 | wc -l | sed 's/ //g') - 1) | sed 's/.\///g'))
+L1dirs=($(find . -type d -maxdepth 1 | tail -n$(expr $(find . -type d -maxdepth 1 | wc -l | sed 's/ //g') - 1) | sed 's/.\///g'))
+tree -L 1 --noreport | tail -n$(expr $(tree -L 1 --noreport | wc -l) - 1) | sed 's/[^a-zA-Z0-9._-]//g'
 printf '%s\n' \
     '---' \
     'layout: page' \
     'title: Assets' \
     'permalink: /assets/' \
     '---' \
-    '### Pages' > README.md
+    '### Pages of Assets' > README.md
 
-for page in "${pages[@]}"; do
-    if [ $page != 'css' ]; then 
+for L1dir in "${L1dirs[@]}"; do
+    if [ $L1dir != 'css' ]; then 
         printf '%s\n' \
-            "1. [$page](/assets/$page)" >> README.md
+            "1. [$L1dir](/assets/$L1dir)" >> README.md
+        cd $L1dir
+        printf '%s\n' \
+            '---' \
+            'layout: page' \
+            "title: Assets/$L1dir" \
+            "permalink: /assets/$L1dir" \
+            '---' \
+            "### Pages of $L1dir" > README.md
+        L2dirs=($(find . -type d -maxdepth 1 | tail -n$(expr $(find . -type d -maxdepth 1 | wc -l | sed 's/ //g') - 1) | sed 's/.\///g'))
+        for L2dir in "${L2dirs[@]}"; do
+            printf '%s\n' \
+                "#### $L2dir" >> README.md
+            files=($(find $L2dir -type f))
+            for file in "${files[@]}"; do
+                printf '%s\n' \
+                    "1. [$file](/assets/$L1dir/$file)" >> README.md
+            done
+        done
+        cd $SCRIPT_DIR/../assets
     fi
 done
